bc5b4d28e19ffad1142a77879f946f29b7d57cbf
DouaeBakkali269 <P138324714@um5.ac.ma>
2025-07-18 02:23:26 +0100

    fix

--- Backend/stage/src/test/java/STAGE/stage/StageApplicationTests.java
+++ Backend/stage/src/test/java/STAGE/stage/StageApplicationTests.java
@@ -3,11 +3,14 @@ package STAGE.stage;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class StageApplicationTestsIT {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
